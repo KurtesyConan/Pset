f790db084d5b77e31ab33cb807ba284aba5da6f9
NKT <prem.nikit@gmail.com>
2023-02-14 15:27:12 +0530

    Pset-4.py

---
+++ Pset-4.py
@@ -0,0 +1,14 @@
+# PSET-4
+
+### In the string `hakuna matata` print the substring `tata` using slicing. Try with both positive and negative indices
+
+# declaring the variable 
+name = "Hakuna Matata"
+# slicing name by using positive slicing
+Positive_Slicing = name[9:13]
+# slicing name by using negetive slicing
+Negetive_Slicing = name[-4:]
+# Printing name by after slicing
+print('name, after positive slicing ' + Positive_Slicing)
+
+print("name, after negetive slicing " + Negetive_Slicing)
