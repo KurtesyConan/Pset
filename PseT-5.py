faaed0c9bdc394e8d209ffa9c57b1af3c575fa10
NKT <prem.nikit@gmail.com>
2023-02-15 12:52:16 +0530

    Pset-5.py

---
+++ PseT-5.py
@@ -0,0 +1,15 @@
+
+### Write a python script that reads the first and last name of a person and returns the full name.###
+
+### requesting for the input of first name ###
+First_Name = input("Enter your first name: ")
+
+### requesting for the input of second name ###
+Second_Name = input("Enter your second name: ")
+
+### summing up first name and second name as full name ###
+Full_Name = First_Name +" "+ Second_Name
+
+
+### printing the full name ###
+print(Full_Name)
