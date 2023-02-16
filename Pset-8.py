81b0001b263b0443bf5c2b2b18917beee93e9adc
NKT <prem.nikit@gmail.com>
2023-02-16 15:38:35 +0530

    Pset-8.py

---
+++ Pset-8.py
@@ -0,0 +1,30 @@
+###In one line of code, display the result of .find() method for the following strings###
+
+##| S No. | string | substring | code | output |
+##| --- | --- | --- | --- | --- |
+##| 1. | 'AAA' | 'a' |  |  |
+##| 2. | 'aaa' | 'a' |  |  |
+##| 3. | 'adasdsfklshfshfd' | 'ds' |  |  |
+##| 4 | 'adasdsfklshfshfd' | 'd' |  |  |###
+
+
+#first string#
+string_1 = "AAA"
+print(string_1.find('a'))
+
+
+#second string#
+string_2 = "aaa"
+print(string_2.find('a'))
+
+
+
+#third string#
+
+string_3 = 'adasdsfklshfshfd'
+print(string_3.find('ds'))
+
+
+#fourth string#
+string_4 = 'adasdsfklshfshfd'
+print(string_4.find('d'))
