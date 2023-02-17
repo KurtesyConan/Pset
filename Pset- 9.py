416871154f4671383fae40b091310670cb3bd836
NKT <prem.nikit@gmail.com>
2023-02-17 13:38:24 +0530

    Pset- 9.py

---
+++ Pset- 9.py
@@ -0,0 +1,10 @@
+### Replace every occurrence of the character "a" with "b" ###
+
+## declaring a variable
+container = "abhfndaakflaa"
+## using replace() method to replace b with a
+container_2 = container.replace("a","b")
+
+print(container_2)
+
+
